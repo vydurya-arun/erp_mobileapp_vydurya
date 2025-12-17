ffcd702d7ef470b44f8ff7966f322bf70031aafc
ARUN KUMAR.A <arun@vydurya.in>
2025-12-17 11:11:56 +0530

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { customFont } from "@/constants/fonts";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts(customFont);
+    useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{headerShown:false}}/>;
 }
